e20318ca94662887858b60533e7ff8ce062af88f
JoeSemper <joe.semper@gmail.com>
2021-11-11 09:42:42 +0300

    Initial commit

---
+++ app/src/main/java/com/joesemper/mymoviesdb/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.joesemper.mymoviesdb.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val PrimaryColor = Color(0xFF0d253f)
+val SecondaryColor = Color(0xFF01b4e4)
+val SupportColor = Color(0xFF90cea1)
